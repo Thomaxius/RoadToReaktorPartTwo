dd294109846e5ad605b94ae76c9b87f70ec67cac
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2019-11-08 00:32:18 +0200

    Change packageParser from class to namespace

--- app/class/packageParser.ts
+++ app/class/packageParser.ts
@@ -3,70 +3,71 @@ import { Dependency } from "./dependency";
 import { AlternativeDependency } from "./alternativeDependency";
 const fs = require('fs');
 
-export abstract class PackageParser {
+export namespace PackageParser {
 
-    static _packages: Package[] = [];
+    let _packages: Package[] = [];
 
-    static fromStatusFile(filePath: string): Package[] {
+    export const fromStatusFile = (filePath: string): Package[] => {
         try {
-            const rawFileData: string = this.fromFile(filePath);
-            this._packages = this.fromFileContents(rawFileData);
-            return this._packages;
+            const rawFileData: string = fromFile(filePath);
+            _packages = fromFileContents(rawFileData);
+            return _packages;
         }
         catch (error) {
             throw new Error(error);
         }
     };
 
-    static fromFile(filePath: string): string {
+    const fromFile = (filePath: string): string => {
         const data: string = fs.readFileSync(filePath, 'UTF-8')
         if (data.length === 0) {
             throw new Error('File is empty');
         }
-        if (this.hasWindowsLineEndings(data)) {
+        if (hasWindowsLineEndings(data)) {
             console.warn("Warning: loaded file contains windows-style line endings. Convert to Linux (LF) if there are problems.")
         }
         return data;
     };
 
-    static hasWindowsLineEndings(data: string) {
+    const hasWindowsLineEndings = (data: string) => {
         return data[data.search(/\r\n/)] !== undefined
     }
 
-    static fromFileContents(fileContents: string): Package[] {
+    const fromFileContents = (fileContents: string): Package[] => {
         const rawDataPerPackage: string[] = fileContents.split('\n\n');
-        this._packages = this.fromRawDataArr(rawDataPerPackage);
-        this.handleDependencies(this._packages);
-        this.sortedAlphabetically(this._packages);
-        return this._packages;
+        _packages = fromRawDataArr(rawDataPerPackage);
+        handleDependencies(_packages);
+        sortedAlphabetically(_packages);
+        return _packages;
     };
 
-    static fromRawDataArr(stringArr: string[]): Package[] {
+    const fromRawDataArr = (stringArr: string[]): Package[] => {
         let packages: Package[] = [];
+
         for (let packageItem of stringArr) {
-            if (!this.isValidPackage(packageItem)) {
+            if (!isValidPackage(packageItem)) {
                 console.debug(`Invalid package, skipping. stringified packageItem: "${JSON.stringify(packageItem)}", length: ${packageItem.length}`);
                 continue;
             }
             const fieldsAndValues: string[] = packageItem.split(/\n(?!\s)/);
-            const _package: Package = this.fromFieldsAndValues(fieldsAndValues);
+            const _package: Package = fromFieldsAndValues(fieldsAndValues);
             packages.push(_package);
         };
         return packages;
     };
 
-    static isValidPackage(packageItem: string) {
+    const isValidPackage = (packageItem: string) => {
         return (packageItem.replace(/\n/g, '').length !== 0);
     }
 
-    static fromFieldsAndValues(fieldsAndValues: string[]): Package {
+    const fromFieldsAndValues = (fieldsAndValues: string[]): Package => {
 
         let argumentsObj = {} as any;
         const extraFields: ExtraFields[] = [];
         argumentsObj.extraFields = extraFields;
 
         fieldsAndValues
-            .filter((_fieldAndValue) => this.isValid(_fieldAndValue))
+            .filter((_fieldAndValue) => isValid(_fieldAndValue))
             .map((validFieldsAndValues) => validFieldsAndValues.split(": ", 2))
             .forEach(([fieldNameInFile, value]) => {
                 const propertyEquivalentObj = fieldNamePropertyEquivalents[fieldNameInFile];
@@ -87,30 +88,30 @@ export abstract class PackageParser {
         return _package;
     }
 
-    static isValid = (_fieldAndValue: string): boolean => _fieldAndValue.length !== 0
+    const isValid = (_fieldAndValue: string): boolean => _fieldAndValue.length !== 0
 
-    static handleDependencies(packages: Package[]): void {
-        this.createDependencies(packages);
-        this.updateDependingPackages(packages);
+    const handleDependencies = (packages: Package[]): void => {
+        createDependencies(packages);
+        updateDependingPackages(packages);
     };
 
-    static createDependencies(packages: Package[]) {
+    const createDependencies = (packages: Package[]) => {
         packages.forEach((_package) => {
-            const preDependencies: Dependency[] = this.dependenciesFromString(_package.preDependenciesString);
-            const dependencies: Dependency[] = this.dependenciesFromString(_package.dependenciesString);
+            const preDependencies: Dependency[] = dependenciesFromString(_package.preDependenciesString);
+            const dependencies: Dependency[] = dependenciesFromString(_package.dependenciesString);
             _package.preDependencies = preDependencies;
             _package.dependencies = dependencies;
         }
         );
     };
 
-    static dependenciesFromString(dependenciesString: string | undefined): Dependency[] {
+    const dependenciesFromString = (dependenciesString: string | undefined): Dependency[] => {
         if (!dependenciesString) {
             return [];
         };
         let dependencies: Dependency[] = [];
 
-        dependenciesString = this.removeVersionNumbers(dependenciesString);
+        dependenciesString = removeVersionNumbers(dependenciesString);
         let dependenciesStringArr: string[] = dependenciesString.split(', ');
 
         for (let dependencyName of dependenciesStringArr) {
@@ -121,10 +122,10 @@ export abstract class PackageParser {
             const hasAlternativeDependencies: boolean = dependencyName.indexOf("|") !== -1;
 
             if (hasAlternativeDependencies) {
-                const _dependency: Dependency = this.dependencyWithAlternativeDependencies(dependencyName);
+                const _dependency: Dependency = dependencyWithAlternativeDependencies(dependencyName);
                 dependencies.indexOf(_dependency) === -1 && dependencies.push(_dependency);
             } else {
-                const _package: Package | undefined = this.getPackageByNameIfExists(dependencyName);
+                const _package: Package | undefined = getPackageByNameIfExists(dependencyName);
                 const _dependency = new Dependency(dependencyName, _package, []);
                 dependencies.indexOf(_dependency) === -1 && dependencies.push(_dependency);
             };
@@ -132,44 +133,44 @@ export abstract class PackageParser {
         return dependencies;
     };
 
-    static removeVersionNumbers(dependencies: string): string {
+    const removeVersionNumbers = (dependencies: string): string => {
         return dependencies.replace(/\s\(.*?\)/g, '');
     };
 
-    static dependencyWithAlternativeDependencies(dependencyName: string): Dependency {
+    const dependencyWithAlternativeDependencies = (dependencyName: string): Dependency => {
         let alternativeDependencyNames: string[] = dependencyName.split(" | ");
         const dependencyToAddAlternativesToName = alternativeDependencyNames[0];
 
-        const _package: Package | undefined = this.getPackageByNameIfExists(dependencyToAddAlternativesToName);
+        const _package: Package | undefined = getPackageByNameIfExists(dependencyToAddAlternativesToName);
 
         let _dependency: Dependency = new Dependency(dependencyToAddAlternativesToName, _package, []);
 
         for (let _alternativeDependencyName of alternativeDependencyNames.splice(1, alternativeDependencyNames.length)) {
-            const _package: Package | undefined = this.getPackageByNameIfExists(_alternativeDependencyName);
+            const _package: Package | undefined = getPackageByNameIfExists(_alternativeDependencyName);
             const _alternativeDependency = new AlternativeDependency(_alternativeDependencyName, _package);
             _dependency.addAlternatives(_alternativeDependency);
         };
         return _dependency;
     };
 
-    static updateDependingPackages(packages: Package[]) {
+    const updateDependingPackages = (packages: Package[]) => {
         packages.forEach((_package) => {
-            const dependingPackages: Package[] = this.getDependingPackages(_package.packageName);
+            const dependingPackages: Package[] = getDependingPackages(_package.packageName);
             const dependingPackageNames: string[] = dependingPackages.map((_package) => _package.packageName) as string[];
             _package.dependingPackageNames = dependingPackageNames;
             _package.dependingPackages = dependingPackages;
         });
     };
 
-    static getPackageByNameIfExists(packageName: string): Package | undefined {
-        const foundPackage = this._packages.find((_package) => _package.packageName === packageName) as Package | undefined;
+    const getPackageByNameIfExists = (packageName: string): Package | undefined => {
+        const foundPackage = _packages.find((_package) => _package.packageName === packageName) as Package | undefined;
         return foundPackage ? foundPackage : undefined;
     };
 
-    static getDependingPackages(packageNameForUpdating: string): Package[] | [] {
+    const getDependingPackages = (packageNameForUpdating: string): Package[] | [] => {
         const dependingPackagesArr: Package[] = [];
 
-        for (const _package of this._packages) {
+        for (const _package of _packages) {
             for (const dependency of _package.dependencies) {
                 if (dependency.packageName === packageNameForUpdating) {
                     dependingPackagesArr.push(_package);
@@ -186,42 +187,43 @@ export abstract class PackageParser {
         return dependingPackagesArr;
     };
 
-    static sortedAlphabetically(packages: Package[]): Package[] {
+    const sortedAlphabetically = (packages: Package[]): Package[] => {
         return packages.sort((a, b) => a.packageName.localeCompare(b.packageName));
     };
 
-    static parseDescription(description: string): Description {
+    const parseDescription = (description: string): Description => {
         const synopsisEndIndex = description.indexOf("\n");
         const synopsis = description.substring(0, synopsisEndIndex);
         let longDescription = description.substring(synopsisEndIndex + 2);
 
         return { synopsis: synopsis, longDescription: longDescription };
     };
+
+    const fieldNamePropertyEquivalents: { [key: string]: { propertyEquivalent: keyof Package, valueParser?: Function } } = {
+        'Package': { propertyEquivalent: 'packageName' },
+        'Status': { propertyEquivalent: 'status' },
+        'Priority': { propertyEquivalent: 'priority' },
+        'Section': { propertyEquivalent: 'section' },
+        'Installed-Size': { propertyEquivalent: 'installedSize', valueParser: parseInt },
+        'Maintainer': { propertyEquivalent: 'maintainer' },
+        'Architecture': { propertyEquivalent: 'architecture' },
+        'Version': { propertyEquivalent: 'version' },
+        'Description': { propertyEquivalent: 'description', valueParser: parseDescription },
+        'Original-Maintainer': { propertyEquivalent: 'originalMaintainer' },
+        'Depends': { propertyEquivalent: 'dependenciesString' },
+        'Pre-Depends': { propertyEquivalent: 'preDependenciesString' },
+        'Source': { propertyEquivalent: 'source' },
+        'Multi-Arch': { propertyEquivalent: 'multiArch' },
+        'Suggests': { propertyEquivalent: 'suggests' },
+        'Homepage': { propertyEquivalent: 'homePage' },
+        'Conflicts': { propertyEquivalent: 'conflicts' },
+        'Conffiles': { propertyEquivalent: 'confFiles' },
+        'Breaks': { propertyEquivalent: 'breaks' },
+        'Recommends': { propertyEquivalent: 'recommends' },
+        'Provides': { propertyEquivalent: 'provides' },
+        'Essential': { propertyEquivalent: 'essential' },
+        'Replaces': { propertyEquivalent: 'replaces' },
+        'Enhances': { propertyEquivalent: 'enhances' }
+    };
 };
 
-const fieldNamePropertyEquivalents: { [key: string]: { propertyEquivalent: keyof Package, valueParser?: Function } } = {
-    'Package': { propertyEquivalent: 'packageName' },
-    'Status': { propertyEquivalent: 'status' },
-    'Priority': { propertyEquivalent: 'priority' },
-    'Section': { propertyEquivalent: 'section' },
-    'Installed-Size': { propertyEquivalent: 'installedSize', valueParser: parseInt },
-    'Maintainer': { propertyEquivalent: 'maintainer' },
-    'Architecture': { propertyEquivalent: 'architecture' },
-    'Version': { propertyEquivalent: 'version' },
-    'Description': { propertyEquivalent: 'description', valueParser: PackageParser.parseDescription },
-    'Original-Maintainer': { propertyEquivalent: 'originalMaintainer' },
-    'Depends': { propertyEquivalent: 'dependenciesString' },
-    'Pre-Depends': { propertyEquivalent: 'preDependenciesString' },
-    'Source': { propertyEquivalent: 'source' },
-    'Multi-Arch': { propertyEquivalent: 'multiArch' },
-    'Suggests': { propertyEquivalent: 'suggests' },
-    'Homepage': { propertyEquivalent: 'homePage' },
-    'Conflicts': { propertyEquivalent: 'conflicts' },
-    'Conffiles': { propertyEquivalent: 'confFiles' },
-    'Breaks': { propertyEquivalent: 'breaks' },
-    'Recommends': { propertyEquivalent: 'recommends' },
-    'Provides': { propertyEquivalent: 'provides' },
-    'Essential': { propertyEquivalent: 'essential' },
-    'Replaces': { propertyEquivalent: 'replaces' },
-    'Enhances': { propertyEquivalent: 'enhances' }
-};
